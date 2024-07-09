3cb442b668d1c6b24748f14c375a85ec996ad84a
JSCoutu <151463469+JSCoutu@users.noreply.github.com>
2024-07-09 11:25:11 -0400

    requis4Fail

---
+++ processor.js
@@ -0,0 +1,11 @@
+function processor(transmission) {
+    if (!transmission.includes("::")) {
+        throw new Error('Data is invalid ; should contain "::"');
+    }
+    let parts = transmission.split("::");
+    return {
+        id: parts[0]
+    };
+}
+
+module.exports = processor;
---
+++ processor.test.js
@@ -0,0 +1,25 @@
+const processor = require('./processor.js');
+
+describe("transmission processor", function() {
+
+    test("takes a string returns an object", function() {
+        let result = processor("9701::<489584872710>");
+        expect(typeof result).toEqual("object");
+    });
+
+    test("throws error if '::' not found", function() {
+        const expectedError = new Error('Data is invalid ; should contain "::"');
+        expect(() => { processor("9701<489584872710>"); }).toThrow(expectedError);
+    });
+
+    test("object contain 'id' as property", function() {
+        let result = processor("9701::<489584872710>");
+        expect(result.id).not.toEqual(undefined);
+    });
+
+    test("'id' property must be Number type"), function() {
+        let result = processor("9701::<489584872710>");
+        expect(typeof result.id)toEqual("Number");
+    }
+ 
+ });
